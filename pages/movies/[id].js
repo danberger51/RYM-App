111fe703681e44816db052ad87ad95016766a4a2
DAN BERGER <dan.berger@ict-campus.net>
2024-06-13 12:58:51 +0200

    css noch überarbeitet

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,55 +1,44 @@
-import MoviesAPI from "../../lib/movies";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import MoviesAPI from '../../lib/movies';
+import Link from 'next/link';
+import styles from './Layout.module.css';
 
-export default function movieDetail() {
-    const router = useRouter()
-    const [movie, setMovie] = useState([])
+export default function MovieDetail() {
+    const router = useRouter();
+    const [movie, setMovie] = useState({});
 
     const handleDelete = async () => {
-        await MoviesAPI.delete(movie.id)
-        router.push(`/`)
-    }
+        await MoviesAPI.delete(movie.id);
+        router.push(`/`);
+    };
+
     useEffect(() => {
         const getMovie = async () => {
-            if (!router.isReady) {
-                return
-            }
-            const response = await MoviesAPI.findById(router.query.id)
-            setMovie(response[0])
-        }
-        getMovie()
+            if (!router.isReady) return;
+            const response = await MoviesAPI.findById(router.query.id);
+            setMovie(response);
+        };
+        getMovie();
     }, [router.isReady]);
 
     return (
-        <div >
-            <div>
-                {
-                    console.log(movie)
-                }
-                <h1>{movie.title}</h1>
-                <p>{movie.description}</p>
-                <p>{movie.director}</p>
-                <p>{movie.year}</p>
-            </div>
-
-            <div >
-                <button><Link className="button" href={`/`}>Back</Link></button>
-                <button><Link className="button" href={`edit/${movie.id}`}>Edit</Link></button>
-                <button><Link className="button" onClick={handleDelete} href={`/`}>Delete</Link></button>
-            </div>
-
+        <div className={styles.container}>
+            {movie && (
+                <>
+                    <div>
+                        <h1>{movie.title}</h1>
+                        <p>{movie.description}</p>
+                        <p>{movie.director}</p>
+                        <p>{movie.year}</p>
+                    </div>
+                    <div>
+                        <button><Link href={`/`}>Back</Link></button>
+                        <button><Link href={`/movies/edit/${movie.id}`}>Edit</Link></button>
+                        <button onClick={handleDelete}>Delete</button>
+                    </div>
+                </>
+            )}
         </div>
-    )
-}
-
-export async function getServerSideProps(context) {
-    const movie = await MoviesAPI.findById(context.params.id)
-    console.log(`der Movie:`, movie)
-    return {
-        props: {
-            movie
-        }
-    }
+    );
 }
